60dcb7b4c5ebdf325470b557c4f9d317fa0a4656
nckim <nckim@forcs.com>
2021-12-21 17:54:20 +0900

    날씨 get

--- src/screens/SecondScreen/index.js
+++ src/screens/SecondScreen/index.js
@@ -1,16 +1,56 @@
-import React from "react";
-import {View, Text} from "react-native";
+import React, {useState} from "react";
+import {View, Text, Button} from "react-native";
+import Geolocation from '@react-native-community/geolocation';
+const API_KEY = "6d0dab065a05231a01cfd61e71406ba9";
+const SecondScreen = () => {
+    const [latitude, setLatitude] = useState(null);
+    const [longitude, setLogitude] = useState(null);
+
+    const geoLocation = () => {
+        Geolocation.getCurrentPosition(
+            position => {
+                const latitude = JSON.stringify(position.coords.latitude);
+                const longitude = JSON.stringify(position.coords.longitude);
+                console.log(position);
+                console.log("lat : " + latitude);
+                console.log("lon : " + longitude);
+                setLatitude(latitude);
+                setLogitude(longitude);
+                const json = getMoviesFromApi();
+                console.log("result json : " + json);
+            },
+            error => { console.log(error.code, error.message); },
+            {enableHighAccuracy:true, timeout: 15000, maximumAge: 10000 },
+        )
 
 
-const SecondScreen = () => (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>hi home</Text>
-        
-        {/* <Post post={post} ></Post> */}
-        {/* <Feed></Feed> */}
+    }
 
+    const getMoviesFromApi = () => {
+        console.log("before");
+        let tt = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+          return json;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+        // console.log("after");
+        return tt;
+      };
 
-    </View>
-)
-
+    return (
+        <View>
+        <Text> latitude: {latitude} </Text>
+            <Text> latitude: {latitude} </Text>
+            <Text> latitude: {latitude} </Text>
+            <Text> latitude: {latitude} </Text>
+            <Text> latitude: {latitude} </Text>
+            <Text> longitude: {longitude} </Text>
+            <Button title="Get GeoLocation" onPress={() => geoLocation()}> </Button>
+        </View>
+    )
+}
 export default SecondScreen;
