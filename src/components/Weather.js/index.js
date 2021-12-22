b043c2cb281e3f9f5ac313363e6ab2692169d238
nckim <nckim@forcs.com>
2021-12-22 17:41:44 +0900

    날씨 카드

--- src/components/Weather.js/index.js
+++ src/components/Weather.js/index.js
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
-import { View, Text, Button, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, Image, TouchableOpacity } from "react-native";
 import styles from "./styles";
 import Geolocation from '@react-native-community/geolocation';
+import FAIcons from 'react-native-vector-icons/FontAwesome';
 
 const API_KEY = "6d0dab065a05231a01cfd61e71406ba9";
 const KELVIN = 273.15;
@@ -12,71 +13,85 @@ const Weather = () => {
     const [temp, setTemp] = useState(null);
     const [tempMin, setTempMin] = useState(null);
     const [tempMax, setTempMax] = useState(null);
+    const [tempFeelsLike, setFeelsLike] = useState(null);
+    const [weatherDescription, setWeatherDescription] = useState(null);
     const [name, setName] = useState(null);
+
+    useEffect(() => {
+        geoLocation();
+    }, []);
+
+    useEffect(() => {
+        const json = getWeatherInfoAsync();
+        console.log(json);
+    }, [longitude]);
 
     const geoLocation = () => {
         Geolocation.getCurrentPosition(
             position => {
                 const latitude = JSON.stringify(position.coords.latitude);
                 const longitude = JSON.stringify(position.coords.longitude);
-                // console.log(position);
-                console.log("lat : " + latitude);
-                console.log("lon : " + longitude);
                 setLatitude(latitude);
                 setLogitude(longitude);
-                const json = getMoviesFromApi();
-                console.log(json);
             },
             error => { console.log(error.code, error.message); },
-            {enableHighAccuracy:true, timeout: 15000, maximumAge: 10000 },
+            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
         )
-
-
     }
 
-    const getMoviesFromApi = () => {
-        console.log("before");
-        let tt = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
-        .then((response) => response.json())
-        .then((json) => {
-            console.log("hi");
+    const getWeatherInfoAsync = async () => {
+        let json;
+        try {
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&lang=kr`
+            );
+            json = await response.json();
             console.log(json);
-            if(json.cod == 200){
-            // const wIcon = json.weather;
-            const wTemp = JSON.stringify(json.main.temp);
-            const wTempMin = JSON.stringify(json.main.temp_min);
-            const wTempMax = JSON.stringify(json.main.temp_max);
-            const wName = JSON.stringify(json.name);
+            if (json.cod == 200) {
+                const wTemp = JSON.stringify(json.main.temp);
+                const wTempMin = JSON.stringify(json.main.temp_min);
+                const wTempMax = JSON.stringify(json.main.temp_max);
+                const wName = JSON.stringify(json.name);
+                const wTempFeelsLike = JSON.stringify(json.main.feels_like);
 
-            setTemp(wTemp - KELVIN);
-            setTempMin(wTempMin - KELVIN);
-            setTempMax(wTempMax - KELVIN);
-            setName(wName);
+                setTemp(wTemp - KELVIN);
+                setTempMin(wTempMin - KELVIN);
+                setTempMax(wTempMax - KELVIN);
+                setName(wName);
+                setFeelsLike(wTempFeelsLike);
 
-            if(json.weather != null){
-                setWeatherIcon(json.weather[0].icon);
+                if (json.weather != null) {
+                    setWeatherIcon(json.weather[0].icon);
+                    setWeatherDescription(json.weather[0].description);
+                }
             }
-            }
-
-            
-          return json;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+        } catch (error) {
+            console.error(error);
+        }
         // console.log("after");
-        return tt;
+        return json;
     };
+
 
     return (
         <View style={styles.container}>
             <View style={styles.weatherContainer}>
-                <Text> 현재온도 : {temp} </Text>
-                <Text> 최저온도 : {tempMin} </Text>
-                <Text> 최고온도 : {tempMax} </Text>
-                <Text> 동네이름 : {name} </Text>
-                <Button title="Get GeoLocation" onPress={() => geoLocation()}> </Button>
-                <Image source={{ uri: `https://openweathermap.org/img/wn/${weatherIcon}@2x.png` }} resizeMode="contain" style={styles.iconStyle} ></Image>
+                <View style={styles.weatherIconTempContainer}>
+                    <Image source={{ uri: `https://openweathermap.org/img/wn/${weatherIcon}@2x.png` }} resizeMode="contain" style={styles.weatherIconStyle} ></Image>
+                    <Text style={styles.curTempTextStyle}>{Math.round(temp)}°</Text>
+                    <Text style={styles.sideTextStyle}>{name}</Text>
+                </View>
+
+                <View style={styles.cityTempContainer}>
+                    <TouchableOpacity onPress={geoLocation}>
+                        <FAIcons on name="refresh" size={20} color={'#000'} />
+                    </TouchableOpacity>
+                    <View style={styles.cityTempStyle}>
+                        <Text style={styles.sideTextStyle}>{weatherDescription}</Text>
+                        <Text style={styles.sideTextStyle}>{Math.round(tempMax)}°/{Math.round(tempMin)}°</Text>
+                        <Text style={styles.sideTextStyle}>체감 {Math.round(tempFeelsLike)}°C</Text>
+                    </View>
+                </View>
             </View>
 
         </View>
--- src/components/Weather.js/styles.js
+++ src/components/Weather.js/styles.js
@@ -1,24 +1,51 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container : {
-        width : '100%',
-        justifyContent: 'space-between', 
+    container: {
+        width: '100%',
+        justifyContent: 'space-between',
         alignItems: 'center',
     },
-    weatherContainer : {
-        width : '80%',
-        justifyContent: 'space-between', 
+    weatherContainer: {
+        flexDirection: 'row',
+        width: '80%',
+        justifyContent: 'space-between',
         alignItems: 'center',
-        backgroundColor : "#03aefe",
-        borderRadius : 20,
-        marginHorizontal : 10
+        backgroundColor: "#03aefe",
+        borderRadius: 20,
+        marginHorizontal: 10
+    },
+    weatherIconTempContainer: {
+        // backgroundColor : "#fffddd",
+        width: '70%',
+        alignItems: 'center',
+        paddingBottom  : 10,
+    },
+    weatherIconStyle: {
+        width : 100,
+        height : 100,
+    },
+    curTempTextStyle: {
+        fontWeight: "500",
+        fontSize: 35,
+    },
 
+    sideTextStyle: {
+        fontSize: 13,
+        marginVertical : 1,
     },
-    iconStyle: {
-        width: 50,
-        height: 50,
-      },
+    
+    cityTempContainer : {
+        width : '30%',
+        justifyContent: 'space-between',
+        alignItems : 'flex-end',
+        paddingRight : 10,
+    },
+    cityTempStyle : {
+        width : '80%',
+        alignItems : 'flex-end',
+        marginVertical : 3,
+    }
 });
 
 export default styles;
